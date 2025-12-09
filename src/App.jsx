60144f8f6bed70d6696442be9e618c2169bfbf6f
SamuelMucham <samuelmucham0607@gmail.com>
2025-12-08 23:27:00 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,142 @@
+
 import { useState, useEffect } from 'react'
-import './App.module.css'
-import {api} from './api/api'
+import s from './App.module.css'
+import { api } from './constants/baseUrl.js'
 import { Card } from './components/card'
-import Tilt from 'react-parallax-tilt'
-import Infomodal from './components/infomodal'
+import { InfoModal } from './components/infoModal'
+import logo from '/logo.webp'
+import { Spinner } from './components/spinner.jsx'
+import ResponsivePagination from 'react-responsive-pagination';
+import 'react-responsive-pagination/themes/classic-light-dark.css';
+
 
 function App() {
   const [data, setData] = useState([])
-  const[searchName, setSearchName] = useState("")
-  const[searchPage, setSearchPage] = useState("")
-  const[modal, setModal] = useState();
+  const [searchName, setSearchName] = useState("")
+  const [searchPage, setSearchPage] = useState("")
+  const [searchStatus, setSearchStatus] = useState("")
+  const [modal, setModal] = useState();
+  const [loading, setLoading] = useState(true);
+  const [erro, setErro] = useState(false);
 
   useEffect(() => {
-    api.get(`/character/?name=${searchName}&page=${searchPage}`).then((response) => {
+    api.get(`/character/?name=${searchName}&page=${searchPage}&status=${searchStatus}`).then((response) => {
       setData(response.data.results)
+      setLoading(false)
+      setErro(false)
     }).catch((error) => {
-      console.errror("nao foi possivel", error)
+      if (error.response.status === 404) {
+        setErro(true)
+        console.error("Error, Characters Not Found!!!", error)
+      }
+      if (error.response.status === 500) {
+        setErro(true)
+        console.error("Error, Internal Server Error!!!", error)
+      }
+      console.error("Error, Could Not Get API!!!", error)
+      setLoading(false)
+      setErro(true)
     })
-  }, [searchPage,searchName])
+  }, [searchPage, searchName, searchStatus])
 
   return (
     <>
-    {modal !== undefined && <Infomodal data={data[modal]} close={() => setModal()}/>}
-      <h1 className={api.title}>rick and morty characters</h1>
+
+     {modal !== undefined && (
+  <InfoModal
+    data={data[modal]}
+    close={() => setModal(undefined)}
+    onNext={() => modal < data.length - 1 && setModal(modal + 1)}
+    onPrev={() => modal > 0 && setModal(modal - 1)}
+    hasNext={modal < data.length - 1}
+    hasPrev={modal > 0}
+  />
+)}
+
       <main>
-        <div style={{display: "flex", gap: "30px", alignItems: "center", justifyContent: "center"}}>
-        <input type="text" value={searchPage} onChange={(e) => setSearchPage(e.target.value)} placeholder='1/42'/>
-        <input type="text" value={searchName} onChange={(e) => setSearchName(e.target.value)} placeholder='Procure um Persongem'/>
+
+        <img src={logo} alt="Logo" className={s.logo} />
+
+        <div className={s.wrapPagination}>
+          <ResponsivePagination
+            current={searchPage}
+            total={42}
+            onPageChange={setSearchPage}
+          />
         </div>
 
-        <div className={api.wrapcards}>
-          {data.map((item, index)=>{
-            return(
-              
-              <div key={index}>
-                <Tilt>
-                <Card image={item.image} name={item.name} species={item.species}/>  
-                </Tilt>
-                <button onClick={() => setModal(index)} className={api.infobtn}>info {item.name}</button>
+        <div className={s.wrapInputs}>
+          <input
+            type="text"
+            value={searchName}
+            onChange={(e) => setSearchName(e.target.value)}
+            placeholder="Search by Name"
+            className={s.inputName}
+          />
+
+          <div className={s.radioGroup}>
+            <label>
+              <input
+                type="radio"
+                value=""
+                checked={searchStatus === ""}
+                onChange={(e) => setSearchStatus(e.target.value)}
+              />
+              All
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="Alive"
+                checked={searchStatus === "Alive"}
+                onChange={(e) => setSearchStatus(e.target.value)}
+              />
+              Alive
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="Dead"
+                checked={searchStatus === "Dead"}
+                onChange={(e) => setSearchStatus(e.target.value)}
+              />
+              Dead
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="Unknown"
+                checked={searchStatus === "Unknown"}
+                onChange={(e) => setSearchStatus(e.target.value)}
+              />
+              Unknown
+            </label>
+          </div>
+        </div>
+
+        {erro &&
+          <div className={s.wrapErro}>
+            <h2>Error when searching for characters ⛔</h2>
+          </div>}
+
+        {loading ? <Spinner /> :
+          <div className={s.wrapCards}>
+            {data.map((item, index) => {
+              return (
+                !erro && <div key={`${searchPage}-${searchName}-${searchStatus}-${index}`} className={s.wrapCard}>
+                  <Card image={item.image} name={item.name} species={item.species} />
+                  <button onClick={() => setModal(index)} className={s.infoBtn}>➕ Info {item.name}</button>
                 </div>
-            )
-          })}
+              )
+            })}
+          </div>
+        }
 
-        </div>
       </main>
+
     </>
   )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,21 +1,10 @@
-import s from './card.module.css'
+import style from './card.module.css'
 
-/*export const Card = (props) => {
+export const Card = ({name, image}) => {
     return(
-        <>
-          <h2>{props.name}</h2>
-                <img src={props.image} alt={props.name} />
-                  <p>{props.species}</p>
-        </>
-    )
-}*/
-
-export const Card = ({image, name, species}) => {
-    return(
-        <div className={s.backgroud}> 
-        <img className={s.cardImage} src={image} alt={name} />
-        <h2 className={s.cardTittle}>{name}</h2>
-        <p className={s.cardText}>{species}</p>
+        <div className={style.cardsStyle}>
+            <h5 className={style.title}>{name}</h5>
+            <img className={style.imgstyle} src={image} alt={name} />
         </div>
     )
 }
